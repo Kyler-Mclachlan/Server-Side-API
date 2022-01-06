6f95da8edc96c5e906dc45f9221a0182ce76d651
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-01-05 20:11:11 -0600

    added basica files and api call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 var getRepoIssues = function(repo) {
     // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=ee74d7c74e74e6fbcc878740bbff7545";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={ee74d7c74e74e6fbcc878740bbff7545}";
   
     // make a get request to url
     fetch(apiUrl).then(function(response) {
