27bf2cfdaf73947c324123d5bfd2a4478dca868d
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-01-07 17:21:33 -0600

    figuring out intial dynamic dom creation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,14 @@
 var submitButtonEl = document.querySelector("#submit-button");
 var cityInputEl = document.querySelector("#exampleCity");
+var cityInfoTopDiv = document.querySelector("#cityInfoTopDiv")
 
 var clickedSubmit = function(event){
     event.preventDefault();
 
     var city = cityInputEl.value.trim();
-    console.log(city)
-}
-
-var getCity = function(repo) {
-    event.preventDefault();
+    
     // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=ee74d7c74e74e6fbcc878740bbff7545";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=ee74d7c74e74e6fbcc878740bbff7545";
   
     // make a get request to url
     fetch(apiUrl).then(function(response) {
@@ -19,6 +16,7 @@ var getCity = function(repo) {
       if (response.ok) {
         response.json().then(function(data) {
             console.log(data)
+            console.log(city)
             listCityConditions(data);
         });
       }
@@ -31,9 +29,13 @@ var getCity = function(repo) {
 
 var listCityConditions = function(stats){
     // add code here to check if city exists
+        var cityContainer = document.createElement("div");
+        cityContainer.classList = "col-12";
         var cityNameEl = document.createElement("span");
         cityNameEl.textContent = stats.name;
-        console.log(cityNameEl)   
+        cityContainer.appendChild(cityNameEl);
+        cityInfoTopDiv.appendChild(cityContainer);
+        console.log(cityNameEl);  
 }
 submitButtonEl.addEventListener("click", clickedSubmit);
 // getCity();
