46b106581b390160064b4ffd4ee2723aa08f7bcb
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-01-06 18:24:42 -0600

    fixing layout

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,16 @@
+var cityHolderEl = document.querySelector("#city-holder");
+
 var getCity = function(repo) {
     // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minuetly,hourly&appid=ee74d7c74e74e6fbcc878740bbff7545";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=ee74d7c74e74e6fbcc878740bbff7545";
   
     // make a get request to url
     fetch(apiUrl).then(function(response) {
       // request was successful
       if (response.ok) {
         response.json().then(function(data) {
-          // displayIssues(data);
             console.log(data)
+            
         });
       }
       else {
@@ -18,4 +20,11 @@ var getCity = function(repo) {
     });
   };
 
-getCity();
+var listCityConditions = function(stats){
+    // add code here to check if city exists
+        var cityNameEl = document.createElement("span");
+        cityNameEl.textContent = stats.name;
+        console.log(cityNameEl)   
+}
+cityHolderEl.addEventListener("submit-button", getCity);
+// getCity();
